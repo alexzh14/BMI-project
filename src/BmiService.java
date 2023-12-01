56156d49ff3950e68c12c6c3b9adf683bc31f899
Alex Zh <alzh147@gmail.com>
2023-12-01 13:23:19 +0600

    Beautification

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public int calculate(int weightKilos, double heightMeters) {
-        double doubleHeight = heightMeters*heightMeters;
-        double bmi = weightKilos/doubleHeight;
+        double doubleHeight = heightMeters * heightMeters;
+        double bmi = weightKilos / doubleHeight;
         int index = (int) bmi;
         return index;
     }
